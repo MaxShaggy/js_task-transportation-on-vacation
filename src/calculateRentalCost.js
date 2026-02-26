763f9a00231be328ad83838e836bea7aef2c0457
Max <maksim.shaggy@gmail.com>
2026-02-26 18:56:29 +0200

    all errors have been fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,21 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let sum = 0;
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
 
-  for (let i = 1; i <= days; i++) {
-    sum += 40;
+  if (days >= LONG_TERM) {
+    return days * PRICE_PER_DAY - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= 3) {
-    sum = sum - 20;
+  if (days >= SHORT_TERM) {
+    return days * PRICE_PER_DAY - SHORT_TERM_DISCOUNT;
   }
 
-  if (days >= 7) {
-    sum = sum - 30;
-  }
-
-  return sum;
+  return days * PRICE_PER_DAY;
 }
 
 module.exports = calculateRentalCost;
